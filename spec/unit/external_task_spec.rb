598593c35ad76c272132cab0c60c774496087078
Duggan Roberts <dugganroberts@gmail.com>
2019-10-25 10:17:35 -0500

    fixed code errors in external_task.spec

--- spec/unit/external_task_spec.rb
+++ spec/unit/external_task_spec.rb
@@ -3,9 +3,10 @@ require 'camunda/model'
 require 'camunda/external_task'
 
 RSpec.describe Camunda::ExternalTask do
+
     it 'fetch tasks' do
-      @tasks = Camunda::ExternalTask.fetch_and_lock(%w[CamundWorkflow])
-      expect(tasks[:reqponse].status).to eq(200)
+     p  @tasks = Camunda::ExternalTask.fetch_and_lock(%w[CamundWorkflow])
+      expect(@tasks[:response].status).to eq(200)
   end
 
   it 'should run fetched tasks' do
