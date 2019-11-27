11bac5cf7a3775f4c4ef22da995ab1d9b08dcdec
Ankur Sethi <ankur.sethi@uscis.dhs.gov>
2019-11-27 11:12:27 -0500

    Correct find_by_business_key_and_task_definition_key!
    Add version file and changelog
    
    Remove mark_task_completed!
    
    Create a shared variable for the process definition key
    Re-arrange vcr cassette names
    Add a Changelog

---
+++ lib/camunda/workflow/version.rb
@@ -0,0 +1,5 @@
+module Camunda
+  module Workflow
+    VERSION = '0.1.1'.freeze
+  end
+end
--- spec/unit/tasks_spec.rb
+++ spec/unit/tasks_spec.rb
@@ -1,16 +1,25 @@
 RSpec.describe Camunda::Task, :vcr, :deployment do
-  context 'mark user tasks completed' do
-    before { Camunda::ProcessDefinition.start_by_key 'CamundaWorkflow', businessKey: 'WorkflowBusinessKey' }
-    let(:subject) { Camunda::Task.mark_task_completed! "WorkflowBusinessTask", "UserTask", {} }
+  let(:business_key) { 'WorkflowBusinessKey' }
+  let(:task_key) { "UserTask" }
+  before { Camunda::ProcessDefinition.start_by_key definition_key, businessKey: business_key }
 
+  # Creating a method instead of a let() because it has to be re-evaluated
+  def task
+    Camunda::Task.find_by_business_key_and_task_definition_key! business_key, task_key
+  end
+  context 'mark user tasks completed' do
+    let(:subject) { task.complete! }
     it('can mark task complete') do
       is_expected.to be_success
-      expect { Camunda::Task.find_by_business_key_and_task_definition_key! "WorkflowBusinessKey", "UserTask" }
-        .to raise_error(Camunda::Task::MissingTask)
+      expect { task }.to raise_error(Camunda::Task::MissingTask)
     end
   end
-  context 'unknown task' do
-    let(:subject) { Camunda::Task.mark_task_completed! "Unknown", "Unknown", {} }
+  context 'unknown task key' do
+    let(:subject) { Camunda::Task.find_by_business_key_and_task_definition_key! business_key, "Unknown" }
+    it('cannot find without task_key') { expect { subject }.to raise_error(Camunda::Task::MissingTask) }
+  end
+  context 'unknown business key' do
+    let(:subject) { Camunda::Task.find_by_business_key_and_task_definition_key! "Unknown", task_key }
     it('cannot mark complete') { expect { subject }.to raise_error(Camunda::Task::MissingTask) }
   end
 end
