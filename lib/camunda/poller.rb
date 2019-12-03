a404597c10acdd33af9177fbe0fa4cbafb3ba7ee
Ankur Sethi <ankur.sethi@uscis.dhs.gov>
2019-12-03 17:48:02 -0500

    Allow Poller to not exit its loop when there is a missing class

--- lib/camunda/poller.rb
+++ lib/camunda/poller.rb
@@ -2,7 +2,11 @@ class Camunda::Poller
   def self.fetch_and_execute(topics, lock_duration: nil, long_polling_duration: nil)
     loop do
       Camunda::ExternalTask
-        .fetch_and_lock(topics, lock_duration: lock_duration, long_polling_duration: long_polling_duration).each(&:queue_task)
+        .fetch_and_lock(topics, lock_duration: lock_duration, long_polling_duration: long_polling_duration).each do |task|
+        task.queue_task
+      rescue Camunda::MissingImplementationClass => e
+        task.failure(e)
+      end
     end
   end
 end
