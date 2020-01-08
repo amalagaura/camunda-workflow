702990f0b62d79dd36fb26db6bf18e841c6d2065
Ankur Sethi <ankur.sethi@uscis.dhs.gov>
2020-01-08 17:20:12 -0500

    Camunda calls the external task from bpmn:endEvent, not the node with bpmn:messageEventDefinition camunda:type="external"

--- spec/camunda/bpmn_xml_spec.rb
+++ spec/camunda/bpmn_xml_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Camunda::BpmnXML do
   describe 'scans file and creates class' do
     it('topics to be an array') { expect(bpmn.topics).to eq %w[CamundaWorkflow CamundaWorkflowErrors] }
     it('expects module_name to be string') { expect(bpmn.to_s).to eq(definition_key) }
+
+    it('shows correct task ids') do
+      expect(bpmn.external_tasks.map(&:class_name)).to eq(%w[DoSomething ServiceTaskForError MessageEndTask])
+    end
   end
 end
