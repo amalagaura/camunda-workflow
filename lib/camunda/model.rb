95bd22d3fa3811ea029d1a8704b18add3bc2b2ad
Danny Kopping <dannykopping@gmail.com>
2020-01-18 14:35:54 +0200

    Checking if Rails is defined before calling it (#38)
    * Checking if Rails is defined before calling it
    
    * Fixing offenses

--- lib/camunda/model.rb
+++ lib/camunda/model.rb
@@ -14,7 +14,9 @@ class Camunda::Model
       c.use Faraday::Request::BasicAuthentication, Camunda::Workflow.configuration.camunda_user,
             Camunda::Workflow.configuration.camunda_password
       # Response
-      c.use Faraday::Response::Logger, ActiveSupport::Logger.new(STDOUT), bodies: true if Rails.env.development?
+      if defined?(Rails)
+        c.use Faraday::Response::Logger, ActiveSupport::Logger.new(STDOUT), bodies: true if Rails.env.development?
+      end
       c.use Her::Middleware::FirstLevelParseJSON
 
       c.use Her::Middleware::SnakeCase
